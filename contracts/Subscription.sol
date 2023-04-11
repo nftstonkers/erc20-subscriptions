dd58cdc224d932b141add01ac6c80f9f77a3359a
Mana <86205866+nftstonkers@users.noreply.github.com>
2023-04-11 21:27:41 +0530

    Update Subscription.sol

--- contracts/Subscription.sol
+++ contracts/Subscription.sol
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+ 
+error INV_ADD();
+error AL_SUB;
 
+ 
 contract Subscription is Pausable, Ownable {
 
     modifier onlyValidAddress {
-        require(msg.sender != address(0), "Invalid creator address");
+        if(msg.sender == address(0))
+            revert INV_ADD();
         _;
     }
 
     modifier canSubscribe(uint256 id) {
-        require(userSubscriptions[msg.sender][id] < block.timestamp, "Already subscribed");
+        if(userSubscriptions[msg.sender][id] >= block.timestamp)
+            revert AL_SUB();
         _;
     }
 
@@ -28,56 +34,23 @@ contract Subscription is Pausable, Ownable {
     SubscriptionInfo[] public subscriptions;
     mapping(address => mapping(uint256 => uint256)) public userSubscriptions;
     
+    event NewSubscription(address indexed owner, uint256 id);
+    event NewSubscriber(address indexed subscriber, address indexed owner, uint256 id, uint256 expiry);
+
     
     function createSubscription(string memory _name, uint256 _cost, uint256 _duration, address _paidTo) external onlyValidAddress whenNotPaused {
         require(_duration>0, "Invalid duration");
         subscriptions.push(SubscriptionInfo(_name,_cost,_duration,_paidTo,msg.sender));
+        emit NewSubscription(msg.sender, subscriptions.length);
+
     }
 
-    function purchase(uint256 id) external payable onlyValidAddress canSubscribe(id) onlyValidAddress whenNotPaused {
+    function purchase(uint256 id) public payable onlyValidAddress canSubscribe(id) onlyValidAddress whenNotPaused {
         require(subscriptions[id].cost == msg.value, "Invalid Amount Received");
         userSubscriptions[msg.sender][id] = block.timestamp + subscriptions[id].period;
         payable(subscriptions[id].paidTo).transfer(msg.value);
+        emit NewSubscriber(msg.sender, subscriptions[id].creator, id, userSubscriptions[msg.sender][id]);
     }
-
-    function getSubscriptionsByUser(address _user) external view returns (SubscriptionInfo[] memory) {
-        uint256 count = subscriptions.length;
-        SubscriptionInfo[] memory userSubscriptionsInfo = new SubscriptionInfo[](count);
-
-        uint256 index = 0;
-        for (uint256 i = 0; i < count; i++) {
-            if (userSubscriptions[_user][i] > 0) {
-                userSubscriptionsInfo[index++] = subscriptions[i];
-            }
-        }
-
-        // Resize the output array to the actual number of subscriptions
-        assembly {
-            mstore(userSubscriptionsInfo, index)
-        }
-
-        return userSubscriptionsInfo;
-    }
-
-    function getSubscriptionsByCreator(address _creator) external view returns (SubscriptionInfo[] memory) {
-        uint256 count = subscriptions.length;
-        SubscriptionInfo[] memory creatorSubscriptionsInfo = new SubscriptionInfo[](count);
-
-        uint256 index = 0;
-        for (uint256 i = 0; i < count; i++) {
-            if (subscriptions[i].creator == _creator) {
-                creatorSubscriptionsInfo[index++] = subscriptions[i];
-            }
-        }
-
-        // Resize the output array to the actual number of subscriptions
-        assembly {
-            mstore(creatorSubscriptionsInfo, index)
-        }
-
-        return creatorSubscriptionsInfo;
-    }
-
 
     function togglePause() external onlyValidAddress onlyOwner {
         paused() ? _unpause() : _pause();
